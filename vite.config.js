d0414c2aabffdc8d076a3ceb1ec60eeda8b88fa9
Tony Villegas <luis.villegas@ipejal.gob.mx>
2026-07-29 13:34:52 -0600

    corrección de url

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/artifex_lunaris/',
+  base: '/artifex.lunaris/',
 })
